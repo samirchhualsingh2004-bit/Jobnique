5cef881a8be2f238cc5c2b8464d7efadab2b3fa2
Samir Chhualsingh <samirchhualsingh2004@gmail.com>
2026-08-19 20:10:39 +0530

    enhanced

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
+  process.env.DB_DATABASE || process.env.DB_NAME,
+  process.env.DB_USERNAME || process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
